2a0fb8a4cd2b5e5b0277c7076c077f56a37ef802
basitaminbhatti <basitaminbhatti@gmail.com>
2022-01-06 11:24:30 +0500

    Adding Intermediate Data Visualization with Seaborn

---
+++ Intermediate Data Visualization with Seaborn/01_Seaborn Introduction/01_Seaborn foundation.py
@@ -0,0 +1,24 @@
+"""======================MCQS====================="""
+# Seaborn foundation
+# What library provides the foundation for pandas and Seaborn plotting?
+
+# Answer the question
+# 50 XP
+# Possible Answers
+
+# javascript
+
+# matplotlib
+
+# vega
+
+# ggplot2
+
+"""ANSWER
+matplotlib"""
+
+
+                    """DEVELOPER"""
+                """BasitAminBhatti"""
+                    """Github""""
+        """https://github.com/basitaminbhatti"""
---
+++ Intermediate Data Visualization with Seaborn/02_Customizing Seaborn Plots/06_Color Palettes.py
@@ -0,0 +1,24 @@
+"""===========================MCQS==========================="""
+# Color Palettes
+# When visualizing multiple elements of data that do not have inherent ordering. Which type of Seaborn palette should you use?
+
+# Answer the question
+# 50 XP
+# Possible Answers
+
+# sequential
+
+# circular
+
+# diverging
+
+# None of the above.
+
+"""Answer
+                  circular"""
+
+
+                    """DEVELOPER"""
+                """BasitAminBhatti"""
+                    """Github""""
+        """https://github.com/basitaminbhatti"""
