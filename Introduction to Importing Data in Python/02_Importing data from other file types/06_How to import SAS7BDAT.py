3c2259d8feccf90b79ea36ecde17d6c82cca6c40
basitaminbhatti <basitaminbhatti@gmail.com>
2022-01-07 11:24:30 +0500

    Adding Introduction to Importing Data in Python

---
+++ Introduction to Importing Data in Python/02_Importing data from other file types/06_How to import SAS7BDAT.py
@@ -0,0 +1,32 @@
+"""===========================MCQ==========================="""
+# How to import SAS7BDAT
+# How do you correctly import the function SAS7BDAT() from the package sas7bdat?
+
+# Answer the question
+# 50 XP
+# Possible Answers
+# import SAS7BDAT from sas7bdat
+# press
+# 1
+# from SAS7BDAT import sas7bdat
+# press
+# 2
+# import sas7bdat from SAS7BDAT
+# press
+# 3
+# from sas7bdat import SAS7BDAT
+# press
+# 4
+
+"""
+ANWSER
+from sas7bdat import SAS7BDAT
+"""
+
+
+#========================================================#
+#                       DEVELOPER                        #
+#                   BasitAminBhatti                      #
+#                        Github                          #
+#           https://github.com/basitaminbhatti           #
+#========================================================#
