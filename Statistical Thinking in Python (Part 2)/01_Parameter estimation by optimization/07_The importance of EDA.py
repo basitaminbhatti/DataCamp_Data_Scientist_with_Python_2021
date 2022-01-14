f0744d3d240820ab6e86f91a4405ad5fcbd2a04b
basitaminbhatti <basitaminbhatti@gmail.com>
2022-01-14 11:24:30 +0500

    Adding Statistical Thinking in Python (Part 2)

---
+++ Statistical Thinking in Python (Part 2)/01_Parameter estimation by optimization/07_The importance of EDA.py
@@ -0,0 +1,29 @@
+"""================MCQ==============="""
+
+# The importance of EDA
+# Why should exploratory data analysis be the first step in an analysis of data (after getting your data imported and cleaned, of course)?
+
+# Answer the question
+# 50 XP
+# Possible Answers
+# You can be protected from misinterpretation of the type demonstrated by Anscombe's quartet.
+
+# EDA provides a good starting point for planning the rest of your analysis.
+
+# EDA is not really any more difficult than any of the subsequent analysis, so there is no excuse for not exploring the data.
+
+# All of these reasons!
+
+
+"""
+ANSWER
+All of these reasons!
+"""
+
+
+#========================================================#
+#                       DEVELOPER                        #
+#                   BasitAminBhatti                      #
+#                        Github                          #
+#           https://github.com/basitaminbhatti           #
+#========================================================#
