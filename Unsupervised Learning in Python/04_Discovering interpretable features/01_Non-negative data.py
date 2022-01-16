d2182dc40d3f57a3f3acdcbd761dae45d56c0e08
basitaminbhatti <basitaminbhatti@gmail.com>
2022-01-16 11:24:30 +0500

    Adding Unsupervised Learning in Python

---
+++ Unsupervised Learning in Python/04_Discovering interpretable features/01_Non-negative data.py
@@ -0,0 +1,30 @@
+"""================MCQ==============="""
+
+# Non-negative data
+# Which of the following 2-dimensional arrays are examples of non-negative data?
+
+# A tf-idf word-frequency array.
+# An array daily stock market price movements (up and down), where each row represents a company.
+# An array where rows are customers, columns are products and entries are 0 or 1, indicating whether a customer has purchased a product.
+# Answer the question
+# 50 XP
+# Possible Answers
+# 1 only
+
+# 2 and 3
+
+# 1 and 3
+
+
+"""
+ANSWER
+ 1 and 3
+"""
+
+
+#========================================================#
+#                       DEVELOPER                        #
+#                   BasitAminBhatti                      #
+#                        Github                          #
+#           https://github.com/basitaminbhatti           #
+#========================================================#
