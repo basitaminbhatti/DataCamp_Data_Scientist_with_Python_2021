6569e58a8644f9fd1ba45e33b650590c73c5d81e
basitaminbhatti <basitaminbhatti@gmail.com>
2022-01-18 11:24:30 +0500

    Adding Cluster Analysis in Python

---
+++ Cluster Analysis in Python/04_Clustering in Real World/06_Clustering with many features.py
@@ -0,0 +1,24 @@
+"""======================MCQ================="""
+# Clustering with many features
+# What should you do if you have too many features for clustering?
+
+# Answer the question
+# 50 XP
+# Possible Answers
+
+# Visualize all the features
+
+# Reduce features using a technique like Factor Analysis
+
+# Perform hierarchical clustering
+
+"""
+Answers
+
+Reduce features using a technique like Factor Analysis"""
+
+
+                    """DEVELOPER"""
+                """BasitAminBhatti"""
+                    """Github""""
+        """https://github.com/basitaminbhatti"""
